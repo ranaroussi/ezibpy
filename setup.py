0cb2793c4f2da2b82452ed470adccba4c01473a7
ran <ran@aroussi.com>
2016-10-26 16:37:52 +0300

    updated version #

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ezIBpy',
-    version="1.12.23",
+    version="1.12.24",
     description='Pythonic Wrapper for IbPy',
     long_description=long_description,
     url='https://github.com/ranaroussi/ezibpy',

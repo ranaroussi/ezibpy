021c6b8ec092969810ddbeca5164be9ff92e39b5
ran <ran@aroussi.com>
2018-09-03 21:39:53 +0300

    changed license to Apache License 2.0

--- ezibpy/__init__.py
+++ ezibpy/__init__.py
@@ -4,13 +4,13 @@
 # ezIBpy: Pythonic Wrapper for IbPy
 # https://github.com/ranaroussi/ezibpy
 #
-# Copyright 2015 Ran Aroussi
+# Copyright 2015-2018 Ran Aroussi
 #
-# Licensed under the GNU Lesser General Public License, v3.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.gnu.org/licenses/lgpl-3.0.en.html
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,7 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.12.59"
+__version__ = "1.12.60"
 __author__ = "Ran Aroussi"
 
 __all__ = ['ezIBpy', 'dataTypes', 'utils']
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ezIBpy',
-    version="1.12.59",
+    version="1.12.60",
     description='Pythonic Wrapper for IbPy',
     long_description=long_description,
     url='https://github.com/ranaroussi/ezibpy',
@@ -31,7 +31,7 @@ setup(
     author_email='ran@aroussi.com',
     license='LGPL',
     classifiers=[
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'License :: OSI Approved :: Apache Software License',
         'Development Status :: 5 - Production/Stable',
 
         'Operating System :: OS Independent',
